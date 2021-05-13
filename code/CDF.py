3948a3231ff7e7a868fdf1799b3e24bdcb59d22c
MrWang98 <w1968953844@163.com>
2021-05-13 16:17:17 +0800

    add CDF,change device type

--- code/CDF.py
+++ code/CDF.py
@@ -25,12 +25,25 @@ else:
     with open(file_name) as f:
         text = f.read()
         text = eval(text)
+names = []
+x=[]
+data=[]
 person={}
 for key in text.keys():
+    names.append(key)
     person[key]={}
+    sum=0
     for n in text[key]:
+        sum+=1
         if n+1 not in person[key]:
             person[key][n+1]=1
         else:
             person[key][n+1]+=1
+    t_y=[]
+    t_x=[]
+    for t_k in person[key]:
+        t_x.append(t_k)
+        t_y.append(person[key][t_k]/sum)
+    x.append(t_x)
+    data.append(t_y)
 print()
